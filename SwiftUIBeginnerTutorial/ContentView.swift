e9c95c6c6bd47c5d1963ffd857edfc3e0436a895
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-12-18 22:00:08 +0530

    Initial Commit

---
+++ SwiftUIBeginnerTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBeginnerTutorial
+//
+//  Created by Mahesh Prasad on 18/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBeginnerTutorial/SwiftUIBeginnerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeginnerTutorialApp.swift
+//  SwiftUIBeginnerTutorial
+//
+//  Created by Mahesh Prasad on 18/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeginnerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
