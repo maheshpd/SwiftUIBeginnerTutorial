3bf01c83acf961a3dca323b99b75b3c3473ad302
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-12-26 21:43:46 +0530

    Advanced TabView, Groups, Image Trim, Divider in SwiftUi

--- SwiftUIBeginnerTutorial/TabViewTemplate.swift
+++ SwiftUIBeginnerTutorial/TabViewTemplate.swift
@@ -9,7 +9,75 @@ import SwiftUI
 
 struct TabViewTemplate: View {
     var body: some View {
-        basicTabView()
+//        basicTabView()
+        tabWithViews()
+    }
+    
+    func tabWithViews() -> some View {
+        TabView {
+            imageSample().tabItem {
+                Image(systemName: "photo.on.rectangle")
+                Text("Images")
+            }
+            dividerSample().tabItem {
+                Image(systemName: "doc.text.fill")
+                Text("Divider")
+            }
+            
+            groupSample().tabItem {
+                Image(systemName: "link")
+                Text("Groups")
+            }
+            
+        }
+    }
+    
+    func groupSample() -> some View {
+        VStack {
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("4")
+            Text("5")
+            Text("6")
+            Text("7")
+            Text("8")
+            Text("9")
+            Group {
+                Text("10")
+                Text("11")
+                Text("12")
+            }.padding()
+                      .foregroundColor(.white)
+                      .background(Color.blue)
+            .cornerRadius(.infinity)
+                      .padding()
+        }
+    }
+    
+    
+    func dividerSample() -> some View {
+        VStack {
+            Divider()
+            Text("Divider 1")
+            Divider().frame(height: 100)
+            HStack {
+                Spacer()
+                Divider().background(Color.orange)
+                Text("Another divider")
+                Divider().background(Color.green)
+                Spacer()
+            }
+        }
+    }
+    
+    
+    func imageSample() -> some View {
+        Image("1")
+            .resizable()
+            .frame(width: 200, height: 200)
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle().trim(from: 0.5, to: 1.0))
     }
     
     func basicTabView() -> some View {
