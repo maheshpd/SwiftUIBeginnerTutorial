081f321313acfa85ff65c40ed9ac33a7ed7bd2f4
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-02 17:58:37 +0530

    @GestureState with Drag Gesture in SwiftUI

--- SwiftUIBeginnerTutorial/DragGestureTemplate.swift
+++ SwiftUIBeginnerTutorial/DragGestureTemplate.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct DragGestureTemplate: View {
     
-    @State private var offset = CGSize.zero
-    @State private var lastDragPosition: DragGesture.Value?
-    @State private var shadowColor = Color.gray
+//    @State private var offset = CGSize.zero
+    @GestureState private var offset = CGSize.zero
+//    @State private var lastDragPosition: DragGesture.Value?
+//    @State private var shadowColor = Color.gray
+    
+    @GestureState private var animation = Animation.linear
     
     var body: some View {
 //        Image("me")
@@ -28,30 +31,43 @@ struct DragGestureTemplate: View {
 //                        }))
 //            .animation(.easeInOut(duration: 1))
         
+//        Image("me")
+//            .resizable()
+//            .frame(width: 100, height: 100)
+//            .offset(offset)
+//            .scaledToFit()
+//            .shadow(color: shadowColor, radius: 20, x: 0, y: 0)
+//            .gesture(DragGesture(minimumDistance: 10).onChanged({ (value) in
+//                self.offset = value.translation
+//                self.lastDragPosition = value
+//            }).onEnded({ (value) in
+//                let timeDiff = value.time.timeIntervalSince(self.lastDragPosition!.time)
+//
+//                let speed = CGFloat(value.translation.height - self.lastDragPosition!.translation.height) / CGFloat(timeDiff)
+//
+//                if speed > 300 {
+//                    self.shadowColor = .red
+//                } else {
+//                    self.shadowColor = .black
+//                }
+//
+//                self.offset = CGSize.zero
+//
+//            }))
+//            .animation(.spring())
+        
+        
         Image("me")
             .resizable()
             .frame(width: 100, height: 100)
             .offset(offset)
-            .scaledToFit()
-            .shadow(color: shadowColor, radius: 20, x: 0, y: 0)
-            .gesture(DragGesture(minimumDistance: 10).onChanged({ (value) in
-                self.offset = value.translation
-                self.lastDragPosition = value
-            }).onEnded({ (value) in
-                let timeDiff = value.time.timeIntervalSince(self.lastDragPosition!.time)
-                
-                let speed = CGFloat(value.translation.height - self.lastDragPosition!.translation.height) / CGFloat(timeDiff)
-                
-                if speed > 300 {
-                    self.shadowColor = .red
-                } else {
-                    self.shadowColor = .black
-                }
-                
-                self.offset = CGSize.zero
-                
+            .shadow(radius: 20)
+            .animation(animation)
+            .gesture(DragGesture(minimumDistance: 10).updating(self.$offset, body: { (value, state, translation) in
+                state = value.translation
+            }).updating(self.$animation, body: { (value, state, translation) in
+                state = Animation.easeInOut(duration: 5)
             }))
-            .animation(.spring())
             
     }
 }
